8b21e032b2adc4ffd4ecafbbe814593de80649fb
Tanishq Singh <tanishqsingh@outlook.in>
2026-09-12 01:23:32 +0530

    Update

--- auto-push.sh
+++ auto-push.sh
@@ -6,15 +6,22 @@ while true; do
     sleep 5
 
     if [ -n "$(git status --porcelain)" ]; then
-        echo "Changes detected. Waiting for changes to settle..."
-
         sleep 5
 
         if [ -n "$(git status --porcelain)" ]; then
             git add .
-            git commit -m "Update"
+
+            FILES=$(git diff --cached --name-only)
+
+            if [ $(echo "$FILES" | wc -l) -eq 1 ]; then
+                FILENAME=$(basename "$FILES")
+                COMMIT_MESSAGE="Update $FILENAME"
+            else
+                COMMIT_MESSAGE="Update multiple files"
+            fi
+
+            git commit -m "$COMMIT_MESSAGE"
             git push
-            echo "✅ Changes committed and pushed."
         fi
     fi
 done
