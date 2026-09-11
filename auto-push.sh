7eac7e507e4061e8414a0cef16903b88ed379bc6
Tanishq Singh <tanishqsingh@outlook.in>
2026-09-12 01:21:24 +0530

    Update

--- auto-push.sh
+++ auto-push.sh
@@ -12,7 +12,7 @@ while true; do
 
         if [ -n "$(git status --porcelain)" ]; then
             git add .
-            git commit -m "push minor update"
+            git commit -m "Update"
             git push
             echo "✅ Changes committed and pushed."
         fi
